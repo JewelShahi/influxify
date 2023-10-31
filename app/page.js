469258e2b1e102104e1cc07646f53c79befe17d0
JoeTheBest <jewelshahi10@gmail.com>
2023-10-31 22:27:30 +0200

    cool design

--- app/page.js
+++ app/page.js
@@ -4,15 +4,17 @@ import HowItWorks from "./components/HowItWorks";
 import About from "./components/About";
 import GlobalPromotion from "./components/GlobalPromotion";
 import Packages from "./components/Packages";
+import Loading from "./loading";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
-      <HowItWorks/>
+      <HowItWorks />
       <About />
       <Packages />
       <GlobalPromotion />
+      <Loading />
     </main>
   );
 }
