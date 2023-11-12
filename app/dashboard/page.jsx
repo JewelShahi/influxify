fe04cd42aa2186fc0b010b55fbf821dbe55c717b
blazed <blazerdork@gmail.com>
2023-11-12 21:03:59 +0200

    patch

---
+++ app/dashboard/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <div>
+        <div>
+          <dir>User Main Info</dir>
+          <dir>Navbar</dir>
+        </div>
+      </div>
+      <div>Account settings</div>
+      <div>Change Password</div>
+    </div>
+  );
+};
+
+export default Page;
